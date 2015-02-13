0fa3e367dfe3a0330feccb503c1dff98b9f46a5b
Nate Coraor <nate@bx.psu.edu>
2015-02-13 15:27:14 -0500

    Bits for PyPI.

---
+++ HISTORY.rst
@@ -0,0 +1,30 @@
+=========
+ History
+=========
+
+0.8
+===
+
+- Add auto-register to ``galaxy start`` if it's called from the root (or
+  subdirectory) of a Galaxy root directory.
+- Make ``galaxycfg remove`` accept instance names as params in addition to
+  config file paths.
+- Use the same hash generated for an instance name as the hash for a generated
+  virtualenv name, so virtualenvs are more easily identified as belonging to a
+  config.
+- Renamed from ``galaxyadmin`` to ``gravity`` (thanks John Chilton).
+
+0.7
+===
+
+- Added the ``galaxyadm`` subcommand ``graceful`` on a suggestion from Nicola
+  Soranzo.
+- Install uWSGI into the config's virtualenv if requested.
+- Removed any dependence on Galaxy and eggs.
+- Moved project to its own repository from the Galaxy clone I'd been working
+  from.
+
+Older
+=====
+
+- Works in progress as part of the Galaxy code.
--- setup.py
+++ setup.py
@@ -1,25 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+long_description += '\n\n'
+with open('HISTORY.rst') as file:
+    long_description += file.read()
+
 
 setup(
     name = 'gravity',
     version = '0.8',
     packages = find_packages(),
     description = 'Manage Galaxy servers',
+    long_description = long_description,
     url = 'https://github.com/galaxyproject/gravity',
     author = 'The Galaxy Team',
     author_email = 'team@galaxyproject.org',
-    license='MIT',
-
+    license = 'MIT',
+    keywords = 'gravity galaxy',
     install_requires = [
         'supervisor',
         'setproctitle',
         'virtualenv'
     ],
-
     entry_points = {
         'console_scripts': [
             'galaxyadm = gravity.cli:galaxyadm',
             'galaxycfg = gravity.cli:galaxycfg'
         ]
-    }
+    },
+    classifiers = [
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ]
 )
