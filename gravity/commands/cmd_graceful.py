3a4a8b6666fd112de889ef87a63c71e2d666f7c7
Nate Coraor <nate@bx.psu.edu>
2022-01-19 14:28:02 -0500

    Don't pretend to do anything with the `graceful` subcommand, which needs to be reworked.

--- gravity/commands/cmd_graceful.py
+++ gravity/commands/cmd_graceful.py
@@ -9,5 +9,6 @@ from gravity import process_manager
 @click.pass_context
 def cli(ctx, instance):
     """Gracefully reload configured services."""
-    with process_manager.process_manager(state_dir=ctx.parent.state_dir) as pm:
-        pm.graceful(instance)
+    raise NotImplementedError("This function has not been updated for Galaxy running with Gunicorn")
+    #with process_manager.process_manager(state_dir=ctx.parent.state_dir) as pm:
+    #    pm.graceful(instance)
