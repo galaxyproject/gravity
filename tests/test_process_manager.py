9ad19884e160abd0b5300bb8a431584e191f072f
Nate Coraor <nate@bx.psu.edu>
2022-06-01 17:19:27 -0400

    Add test for disabling services that default to enabled.

--- tests/test_process_manager.py
+++ tests/test_process_manager.py
@@ -85,6 +85,24 @@ def test_update_force(galaxy_yml, default_config_manager):
     assert gunicorn_conf_path.stat().st_mtime != update_time
 
 
+def test_disable_services(galaxy_yml, default_config_manager):
+    default_config_manager.add([str(galaxy_yml)])
+    galaxy_yml.write(json.dumps(
+        {'galaxy': None, 'gravity': {
+            'gunicorn': {'enable': False},
+            'celery': {'enable': False, 'enable_beat': False}}}
+    ))
+    with process_manager.process_manager(state_dir=default_config_manager.state_dir) as pm:
+        pm.update()
+    instance_conf_dir = Path(default_config_manager.state_dir) / 'supervisor' / 'supervisord.conf.d' / '_default_.d'
+    gunicorn_conf_path = instance_conf_dir / "galaxy_gunicorn_gunicorn.conf"
+    assert not gunicorn_conf_path.exists()
+    celery_conf_path = instance_conf_dir / "galaxy_celery_celery.conf"
+    assert not celery_conf_path.exists()
+    celery_beat_conf_path = instance_conf_dir / "galaxy_celery-beat_celery-beat.conf"
+    assert not celery_beat_conf_path.exists()
+
+
 @pytest.mark.parametrize('job_conf', [[JOB_CONF_XML_DYNAMIC_HANDLERS]], indirect=True)
 def test_dynamic_handlers(default_config_manager, galaxy_yml, job_conf):
     galaxy_yml.write(DYNAMIC_HANDLER_CONFIG)
