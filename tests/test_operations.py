9042078b11d37d203c818f6de3b6a868d09398fd
Nate Coraor <nate@bx.psu.edu>
2022-08-25 10:28:29 -0400

    When testing for the celery beat schedule db, search all known extensions.

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -9,9 +9,12 @@ from click.testing import CliRunner
 from yaml import safe_load
 
 from gravity.cli import galaxyctl
+from gravity.state import CELERY_BEAT_DB_FILENAME
 
 STARTUP_TIMEOUT = 30
 CELERY_BEAT_TIMEOUT = 10
+# celery.beat.PersistentScheduler uses shelve, which can append a suffix based on which db backend is used
+CELERY_BEAT_DB_FILENAMES = map(lambda ext: CELERY_BEAT_DB_FILENAME + ext, ('', '.db', '.dat', '.bak', '.dir'))
 
 
 def test_cmd_register(state_dir, galaxy_yml):
@@ -52,10 +55,10 @@ def wait_for_gxit_proxy(state_dir):
     return startup_logs
 
 
-def wait_for_path(path, timeout):
+def wait_for_any_path(paths, timeout):
     for _ in range(timeout * 4):
         try:
-            assert path.exists()
+            assert any(map(lambda x: x.exists(), paths))
             return True
         except AssertionError:
             time.sleep(0.25)
@@ -80,7 +83,8 @@ def test_cmd_start(state_dir, galaxy_yml, startup_config, free_port):
     assert result.exit_code == 0, result.output
     start_instance(state_dir, free_port)
     result = runner.invoke(galaxyctl, ['--state-dir', state_dir, 'status'])
-    celery_beat_db_exists = wait_for_path(state_dir / "celery-beat-schedule", CELERY_BEAT_TIMEOUT)
+    celery_beat_db_paths = map(lambda f: state_dir / f, CELERY_BEAT_DB_FILENAMES)
+    celery_beat_db_exists = wait_for_any_path(celery_beat_db_paths, CELERY_BEAT_TIMEOUT)
     assert celery_beat_db_exists is True, "celery-beat failed to write db. State dir contents:\n" \
         f"{os.listdir(state_dir)}"
     result = runner.invoke(galaxyctl, ['--state-dir', state_dir, 'stop'])
