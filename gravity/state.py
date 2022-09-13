04ed830cc05d1594ff11868624d62e40fae0450f
Nate Coraor <nate@bx.psu.edu>
2022-09-13 16:50:46 -0400

    Convert pre-1.0 configstate. Drops conversion from json, which was only used by Old Gravity™ many years ago and is incredibly unlikely to exist in the wild.

--- gravity/state.py
+++ gravity/state.py
@@ -9,6 +9,8 @@ from collections import defaultdict
 
 import yaml
 
+from gravity import __version__
+from gravity.io import debug
 from gravity.util import AttributeDict
 
 
@@ -185,6 +187,12 @@ class GalaxyStandaloneService(Service):
 
 
 class ConfigFile(AttributeDict):
+    persist_keys = (
+        "config_type",
+        "instance_name",
+        "galaxy_root",
+    )
+
     def __init__(self, *args, **kwargs):
         super(ConfigFile, self).__init__(*args, **kwargs)
         services = []
@@ -212,38 +220,42 @@ class ConfigFile(AttributeDict):
 
 
 class GravityState(AttributeDict):
+    init_contents = {
+        "gravity_version": __version__,
+        "config_files": {},
+    }
+
     @classmethod
     def open(cls, name):
         try:
             s = cls.loads(open(name).read())
         except (OSError, IOError) as exc:
             if exc.errno == errno.ENOENT:
-                yaml.dump({}, open(name, "w"))
-                s = cls()
+                debug(f"Initializing Gravity config state: {name}")
+                yaml.dump(GravityState.init_contents, open(name, "w"))
+                s = cls(**GravityState.init_contents)
         s._name = name
         return s
 
     def __init__(self, *args, **kwargs):
         super(GravityState, self).__init__(*args, **kwargs)
         normalized_state = defaultdict(dict)
-        for key in ("config_files",):
-            if key not in self:
-                self[key] = {}
-            for config_file, config_dict in self[key].items():
-                # resolve path, so we always deal with absolute and symlink-resolved paths
-                config_file = os.path.realpath(config_file)
-                if config_file.endswith(GALAXY_YML_SAMPLE_PATH):
-                    root_dir = config_dict['galaxy_root']
-                    non_sample_path = os.path.join(root_dir, 'config', 'galaxy.yml')
-                    if os.path.exists(non_sample_path):
-                        config_file = non_sample_path
-                normalized_state[key][config_file] = ConfigFile(config_dict)
+        for config_file, config_dict in self["config_files"].items():
+            # resolve path, so we always deal with absolute and symlink-resolved paths
+            config_file = os.path.realpath(config_file)
+            if config_file.endswith(GALAXY_YML_SAMPLE_PATH):
+                root_dir = config_dict['galaxy_root']
+                non_sample_path = os.path.join(root_dir, 'config', 'galaxy.yml')
+                if os.path.exists(non_sample_path):
+                    config_file = non_sample_path
+            normalized_state["config_files"][config_file] = ConfigFile(config_dict)
         self.update(normalized_state)
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        self["gravity_version"] = __version__
         with open(self._name, "w") as fh:
             self.dump(fh)
 
