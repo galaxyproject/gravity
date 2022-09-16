afd496ccdde07502e6f42ae1b4127d130aed050c
Nate Coraor <nate@bx.psu.edu>
2022-09-16 12:16:06 -0400

    Reset docs theme, RTD should override

--- docs/conf.py
+++ docs/conf.py
@@ -31,5 +31,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'default'
 html_static_path = ['_static']
