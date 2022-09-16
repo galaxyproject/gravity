a9b225d033d0462f47e4adecef2ef90fc0bf2318
Nate Coraor <nate@bx.psu.edu>
2022-09-16 12:04:08 -0400

    Correct path to parent dir

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from gravity import __version__  # noqa: E402
 
