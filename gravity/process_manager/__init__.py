a7e0ac39bbcc2131d0619f2cd0c55a347319ba2d
Nate Coraor <nate@bx.psu.edu>
2022-10-12 13:08:47 +0200

    Convert os.environ to a dict for union

--- gravity/process_manager/__init__.py
+++ gravity/process_manager/__init__.py
@@ -233,7 +233,7 @@ class ProcessExecutor(BaseProcessExecutionEnvironment):
         print_env = ' '.join('{}={}'.format(k, shlex.quote(v)) for k, v in format_vars["environment"].items())
 
         cmd = shlex.split(format_vars["command"])
-        env = format_vars["environment"] | os.environ
+        env = format_vars["environment"] | dict(os.environ)
         cwd = format_vars["galaxy_root"]
 
         info(f"Working directory: {cwd}")
