77cb28bf0956c61d09e33bfd562c7718d4045490
Nate Coraor <nate@bx.psu.edu>
2022-10-21 15:20:25 -0400

    Correct option name

--- gravity/commands/cmd_graceful.py
+++ gravity/commands/cmd_graceful.py
@@ -7,7 +7,7 @@ from gravity import process_manager
 @click.command("graceful")
 @options.instances_services_arg()
 @click.pass_context
-def cli(ctx, instance_services):
+def cli(ctx, instances_services):
     """Gracefully reload configured services.
 
     If no INSTANCES or SERVICES are provided, all configured services of all configured instances are gracefully
@@ -16,4 +16,4 @@ def cli(ctx, instance_services):
     Specifying INSTANCES and SERVICES limits the operation to only the provided instance name(s) and/or service(s).
     """
     with process_manager.process_manager(**ctx.parent.cm_kwargs) as pm:
-        pm.graceful(instance_names=instance_services)
+        pm.graceful(instance_names=instances_services)
