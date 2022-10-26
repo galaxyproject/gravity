97c5e87b86eda86055d68a96f9dba28549584f73
Nate Coraor <nate@bx.psu.edu>
2022-10-26 15:21:24 -0400

    Add requests-unixsocket as a conditional dependency so that Galaxy doesn't pull it.

--- setup.py
+++ setup.py
@@ -37,7 +37,10 @@ setup(
     license="MIT",
     keywords="gravity galaxy",
     python_requires=">=3.6",
-    install_requires=["Click", "supervisor", "pyyaml", "packaging", "pydantic", "jsonref"],
+    install_requires=["Click", "supervisor", "pyyaml", "packaging", "pydantic", "jsonref", "requests"],
+    extras_require={
+        "unixsocket": ["requests-unixsocket"],
+    },
     entry_points={"console_scripts": [
         "galaxy = gravity.cli:galaxy",
         "galaxyctl = gravity.cli:galaxyctl",
