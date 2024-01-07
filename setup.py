46d5ce47a2f415e9be6a048f948b1f423794da6e
mvdbeek <m.vandenbeek@gmail.com>
2024-01-07 17:33:26 +0100

    Update supported python versions

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     author_email="team@galaxyproject.org",
     license="MIT",
     keywords="gravity galaxy",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=[
         "Click",
         "supervisor",
@@ -56,10 +56,10 @@ setup(
         "Natural Language :: English",
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     zip_safe=False,
 )
